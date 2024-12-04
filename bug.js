d8cdeff6e5cbc28bcfdd7881518719da26b66f8d
BugHunter-X <askzenai@gmail.com>
2024-12-04 16:52:09 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,24 @@
+```javascript
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState({ count: this.state.count + 1 });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    return <div>Count: {this.state.count}</div>;
+  }
+}
+```
